42117b54c859be02bc878768b0fa0beb0c45a8fb
Leon-SA <sleon08@hotmail.es>
2017-10-23 18:46:39 -0600

    Tengo que preguntar por el 2.

--- Ejercicio2.m
+++ Ejercicio2.m
@@ -1,36 +1,57 @@
 % Ejercicio 2
 % Filtro ranura de sólo ceros (FIR).
 
-w = -pi:0.01:pi; % Frecuencia.
+w = -pi:0.0001:pi; % Frecuencia.
 
 b0 = 1/(2+sqrt(2)); % Ganancia.
 w0 = pi/4; % Frecuencia de los ceros conjugados.
 
-% Funcion de magnitud |H(w)|.
+% Funcion |H(w)|.
 V1 = 2-2.*cos(w-w0);
 V2 = 2-2.*cos(w+w0);
 H = abs(b0).*sqrt(V1.*V2);
 
-% Funcion de fase de H(w).
+% Funcion <H(w).
 A = sin(w-w0);
 B = 1-cos(w-w0);
 C = sin(w+w0);
 D = 1-cos(w+w0);
-P = atan(A./B);%+atan(C./D);
+PH = atan(A./B)+atan(C./D);
 
-Y = sqrt(2)/2;
-PY = -w;
+% Salida inciso a.
+%Xa =
+%PXa =
+%Ya =
+%PYa =
 
-% Grafica de magnitud.
+% Salida inciso b.
+Xb = (sqrt(2)/2).*(1 ./sqrt(2-2 .*cos(w)));
+PXb = -atan(sin(w)./(1-cos(w)));
+Yb = H.*Xb;
+PYb = PH+PXb;
+
+% Graficas de H(w).
 figure;
+subplot(1,2,1);
 plot(w, H);
+title('Respuesta en magnitud del filtro')
+xlabel('w');
+ylabel('|H(w)|');
+subplot(1,2,2);
+plot(w, PH);
+title('Respuesta en fase del filtro')
+xlabel('w');
+ylabel('<H(w)');
 
-% Grafica de fase.
+%  Graficas de salida inciso b.
 figure;
-plot(w, P);
-
-figure;
-plot(w, Y);
-
-figure;
-plot(w, PY);
+subplot(1,2,1);
+plot(w, Yb);
+title('Respuesta en magnitud de salida inciso b')
+xlabel('w');
+ylabel('|Y(w)|');
+subplot(1,2,2);
+plot(w, PYb);
+title('Respuesta en fase de salida inciso b')
+xlabel('w');
+ylabel('<H(w)');
