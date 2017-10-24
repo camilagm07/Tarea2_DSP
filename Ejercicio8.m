f6dee9df8c0a43ad99b19dd7acdd4d496e21e2ba
Leon-SA <sleon08@hotmail.es>
2017-10-23 23:07:34 -0600

    8 listo :D.

--- Ejercicio8.m
+++ Ejercicio8.m
@@ -1,7 +1,28 @@
-w = -pi:0.0001:pi;
+% Ejercicio 8
+% Filtro peine a partir de hendidura.
 
-a = 0.9;
+w = -pi:0.0001:pi; % Frecuencia.
+a = 0.9; % Radio de los polos.
 
-H = sqrt(2-2.*cos(8.*w))./sqrt(1 - 2*a .*cos(8.*w) + a.^2);
+% Funcion |H(w)|.
+H = sqrt(2-2 .*cos(w))./sqrt(1-2*a .*cos(w) + a^2);
 
+% Funcion |H8(w)|.
+H8 = sqrt(2-2 .*cos(8*w))./sqrt(1-2*a .*cos(8*w) + a^2);
+
+% Graficas.
+figure;
+subplot(1,2,1);
 plot(w, H);
+title('Respuesta en magnitud del filtro hendidura')
+xlabel('w');
+ylabel('|H(w)|');
+subplot(1,2,2);
+plot(w, H8);
+title('Respuesta en magnitud del filtro peine')
+xlabel('w');
+ylabel('|H8(w)|');
+
+
+
+
