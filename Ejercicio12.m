6ed5e94b3a4535b0a70e53d37a4e11bdfda6da90
Leon-SA <sleon08@hotmail.es>
2017-10-24 00:39:33 -0600

    12 lesto.

--- Ejercicio12.m
+++ Ejercicio12.m
@@ -1,27 +1,28 @@
-w0 = pi/3;
-a1 = 2*cos(w0);
-a2 = 1;
-b0 = 1;
+% Ejercicio 12
+% Oscilador sinusoidal digital.
 
-%n = (0:1:500);
+% Constantes.
+b0 = 1; % Impulso unitario.
+w0 = pi/3; % Frecuencia de señal discreta.
+a1 = -2*cos(w0);
 
-for n = 3:1:501
-  if (n == 3)
-    x(n) = 1;
-  else
-    x(n) = 0;
-  endif
+%Condiciones iniciales
+y1 = 1;
+y2 = 0;
+
+for n = (1:1:61) % Algoritmo recursivo.
+  y(n) = b0 - a1*y1 - y2;
   
-  if (n == 3)
-    y(n-1) = 1;
-    y(n-2) = 1;
-  endif
-    y(n) = -a1*y(n-1)-a2*y(n-2)+b0*x(n);
-endfor
+  b0 = 0; % Solo en primer n vale 1.
+  % Cambio de condiciones.
+  y2 = y1;
+  y1 = y(n);
+  endfor
 
-figure;
-hold on;
-for n = 3:1:100;
-  stem(n, y(n));
-endfor
-hold off;
+%Graficacion
+stem(n= 1:61, y, '.');
+title('Oscilador digital con periodo N = 6')
+grid;
+axis([-1,63,-2.2,2.2]);
+xlabel('n');
+ylabel('y(n)');
