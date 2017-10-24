9f1b8e60b49865c4b92f6593a4b68bf6fc596dee
Leon-SA <sleon08@hotmail.es>
2017-10-24 00:39:23 -0600

    10 lesto.

--- Ejercicio10.m
+++ Ejercicio10.m
@@ -1,9 +1,23 @@
-w = (-pi:0.0001:pi)';
+% Ejercicio 10
+% Filtros inversos.
 
-H = sqrt(1.9025-1.9*cos(6*w));
-HI = 1/H;
+w = -pi:0.0001:pi; % Frecuencia.
 
+% Funcion |H(w)|.
+H = sqrt(1.9025-1.9 .*cos(6*w));
+
+% Funcion |HI(w)|.
+HI = 1 ./H;
+
+% Graficas.
 figure;
+subplot(1,2,1);
 plot(w, H);
-figure;
+title('Respuesta en magnitud del filtro')
+xlabel('w');
+ylabel('|H(w)|');
+subplot(1,2,2);
 plot(w, HI);
+title('Respuesta en magnitud del filtro inverso')
+xlabel('w');
+ylabel('|HI(w)|');
