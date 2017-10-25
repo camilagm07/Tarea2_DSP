57f5c15aa0649f6332d2ff1dea1abd7bd44c65fc
Leon-SA <sleon08@hotmail.es>
2017-10-24 23:22:05 -0600

    Cambiecillos al 12.

--- Ejercicio12.m
+++ Ejercicio12.m
@@ -6,21 +6,21 @@ b0 = 1; % Impulso unitario.
 w0 = pi/3; % Frecuencia de señal discreta.
 a1 = -2*cos(w0);
 
-%Condiciones iniciales
+%Condiciones iniciales.
 y1 = 1;
 y2 = 0;
 
 for n = (1:1:61) % Algoritmo recursivo.
   y(n) = b0 - a1*y1 - y2;
   
-  b0 = 0; % Solo en primer n vale 1.
   % Cambio de condiciones.
+  b0 = 0; % Solo en primer n vale 1.
   y2 = y1;
   y1 = y(n);
   endfor
 
-%Graficacion
-stem(n= 1:61, y, '.');
+%Graficacion.
+stem(n = 0:1:60, y(n+1), '.'); % Se corre para que empiece en n = 0.
 title('Oscilador digital con periodo N = 6')
 grid;
 axis([-1,63,-2.2,2.2]);
