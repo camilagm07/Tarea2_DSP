79f271e88bf6ec3d77cba15510538d21b5035ebc
Camila Gómez <camilagm994@gmail.com>
2017-10-25 23:26:58 -0600

    Ejercicios 5 y 9 revisados

--- Ejercicio5.m
+++ Ejercicio5.m
@@ -1,19 +1,22 @@
 %Ejercicio 5
+% y(n) = b0x(n)+b1x(n-1)+b2x(n-2)
+% Calcular coeficientes para que el filtro rechace wo=2pi/3
+% H(0) = 1
+
+w = [-pi:0.004:pi]; % Frecuencia w desde -pi a pi con 500pi puntos.
+s = size(w); % Tamaño de matriz w.
 
 
-w = [-pi:0.004:pi];
+b0 = 1/3; %Normalización, valor de los coeficientes
 
-
-%Cálculo de magnitud
-b0 = 1/3;
-magA = abs(b0.*(1+exp(-j*w)+exp(-2*j*w)));
-
-%Calculo de fase
+%Calculo de Magnitud y fase
 Real = 1+cos(w)+cos(2*w);
 Imaginaria = -sin(w)-sin(2*w);
-s = size(w);
 [Magnitud,Phase] = Fase(s,Real,Imaginaria);
 
+Magnitud = Magnitud*b0;
+
+%Figura
 
 figure(1)
 subplot(1,2,1);
--- Ejercicio9.m
+++ Ejercicio9.m
@@ -6,23 +6,27 @@
 
 f = [-1:0.004:1];
 w = 3.14*f;
-w0 = 120*pi;
+w0 = 0.24*pi;
 
 % Inciso A: filtro de segundo orden 
-b0 = 1/4;
+b0 = 0.2892;
+
+
 % Funcion de magnitud |H(w)|
 
 V1 = 2-2.*cos(w-w0);
 V2 = 2-2.*cos(w+w0);
 H = abs(b0).*sqrt(V1.*V2);
 
-% Funcion de fase de H(w).
+% Funcion de fase de H(w)
+
 A = sin(w-w0);
 B = 1-cos(w-w0);
 C = sin(w+w0);
 D = 1-cos(w+w0);
-P = atan(A./B);%+atan(C./D);
-
+P1 = atan(A./B);
+P2 = atan(C./D);
+P3 = P1+P2;
 
 figure(1)
 subplot(1,2,1);
@@ -32,13 +36,13 @@ xlabel('w');
 ylabel('|Ha(jw)|');
 
 subplot(1,2,2);
-plot(w,P,"m");
+plot(w,P3,"m");
 title("Fase filtro ranura FIR de segundo orden")
 xlabel('w');
 ylabel('<Ha(jw)');
 
 % Inciso B: filtro de segundo orden con polos r = 0.9
-b1 = 361/400;
+b1 = 0.9030;
 
 % Funcion de magnitud |H(w)|
 
@@ -53,8 +57,9 @@ A1 = 0.9*sin(w-w0);
 B1 = 1-0.9*cos(w-w0);
 C1 = 0.9*sin(w+w0);
 D1 = 1-0.9*cos(w+w0);
-P1 = atan(A1./B1);%+atan(C1./D1);
-PT = P-P1; %Le resto a la fase de los ceros la fase de los polos
+P4 = atan(A1./B1);
+P5 = atan(C1./D1);
+PT = P3-(P4+P5); %Le resto a la fase de los ceros la fase de los polos
 
 figure(2)
 subplot(1,2,1);
