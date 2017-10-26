fea7fe5a9cb360df5aea696eaa79f7ffbead9ec9
Camila Gómez <camilagm994@gmail.com>
2017-10-25 23:08:35 -0600

    Ejercicio 3 revisado

--- Ejercicio3.m
+++ Ejercicio3.m
@@ -1,8 +1,11 @@
 %Ejercicio 3
 
+%Filtro ranura suprime señales constantes, es decir señales con w= 0
+%El filtro debe tener un polo con r = 0.9 y un cero
 
-w = [-pi:0.004:pi];
-s = size(w);
+
+w = [-pi:0.004:pi]; % Frecuencia w desde -pi a pi con 500pi puntos.
+s = size(w); % Tamaño de matriz w. 
 
 RealNumerador = 1-cos(w);
 RealDenominador = 1+0.9*cos(w);
@@ -13,8 +16,8 @@ ImaginarioDenominador = -0.9*sin(w);
 [MagnitudN,FaseN] = Fase(s,RealNumerador,ImaginarioNumerador);
 [MagnitudD,FaseD] = Fase(s,RealDenominador,ImaginarioDenominador);
 
-Magnitud = MagnitudN./MagnitudD;
-Fase2 = FaseN.-FaseD;
+Magnitud = MagnitudN./MagnitudD; %Magnitud funcion de transferencia
+Fase2 = FaseN.-FaseD; %Fase de la funcion de transferencia
 
 figure(1)
 subplot(1,2,1);
@@ -32,15 +35,16 @@ ylabel('<H(jw)');
 % c) Normalizada
 bo = 1/20;
 Magnitud2 = Magnitud*bo;
+
 figure(2)
 subplot(1,2,1);
-plot(w,Magnitud2,"m");
-title("Magnitud filtro rechaza wo=0")
+plot(w,Magnitud2,"b");
+title("Magnitud filtro rechaza wo=0 normalizada")
 xlabel('w');
 ylabel('|H(jw)|');
 
 subplot(1,2,2);
-plot(w,Fase2,"m");
+plot(w,Fase2,"b");
 title("Fase filtro rechaza wo=0")
 xlabel('w');
 ylabel('<H(jw)'); 
@@ -51,6 +55,8 @@ n = [0:0.1:30];
 x = 2*cos(pi/2*n+pi/4);
 xanterior = 0;
 yanterior = 0;
+
+% Calculo recursivo para obtener y(n), se utiliza n = 30.
 
 for i=1:301
   y(i) = 1/20*x(i)-1/20*xanterior-0.9*yanterior;
